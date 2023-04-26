b1e79fe243fc326f42e78f54f1466e055e0c4852
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2023-04-26 01:45:27 -0500

    feat: use only columns
    <body>
    
    <footer>

--- bigqueryPorvenir/repository/invoice/InvoiceSiigoRespository.go
+++ bigqueryPorvenir/repository/invoice/InvoiceSiigoRespository.go
@@ -23,22 +23,19 @@ func InsertInvoice(ctx context.Context, dataset bigquery.Dataset, listsSiigo sii
 	var listBigQuery []bigquery_invoice.InvoiceSiigo
 	for _, item := range listsSiigo.Invoices {
 		invoice := bigquery_invoice.InvoiceSiigo{
-			ID:       item.ID,
-			Document: getDocument(item, listsSiigo),
-			Prefix:   item.Prefix,
-			Number:   item.Number,
-			Name:     item.Name,
-			Date:     item.Date,
-			Customer: getCustomer(item, listsSiigo),
-			Seller:   item.Seller,
-			Total:    item.Total,
-			Balance:  item.Balance,
-			Stamp:    bigquery_invoice.Stamp(item.Stamp),
-			Mail:     bigquery_invoice.Mail(item.Mail),
-			Metadata: bigquery_invoice.Metadata{
-				Created:     item.Metadata.Created,
-				LastUpdated: util_bigquery.SetStringToEmptyIfNil(item.Metadata.LastUpdated),
-			},
+			ID:                 item.ID,
+			Name:               item.Name,
+			Date:               item.Date,
+			CustomerName:       getCustomerName(item, listsSiigo),
+			CustomerIdTypeCode: getCustomerIdTypeCode(item, listsSiigo),
+			CustomerIdTypeName: getCustomerNameIdTypeName(item, listsSiigo),
+			Total:              item.Total,
+			StampStatus:        item.Stamp.Status,
+			StampCufe:          item.Stamp.Cufe,
+			MailStatus:         item.Mail.Status,
+			MailObservations:   item.Mail.Observations,
+			Created:            item.Metadata.Created,
+			LastUpdated:        util_bigquery.SetStringToEmptyIfNil(item.Metadata.LastUpdated),
 		}
 		for _, payment := range item.Payments {
 			invoice.Payments = append(invoice.Payments, bigquery_invoice.Payment{
@@ -49,12 +46,10 @@ func InsertInvoice(ctx context.Context, dataset bigquery.Dataset, listsSiigo sii
 		}
 		for _, itemInvoice := range item.Items {
 			invoice.Items = append(invoice.Items, bigquery_invoice.Item{
-				Product:     getProduct(itemInvoice, listsSiigo),
-				Quantity:    itemInvoice.Quantity,
-				Price:       itemInvoice.Price,
-				Description: itemInvoice.Description,
-				Taxes:       getTaxes(itemInvoice.Taxes),
-				Total:       itemInvoice.Total,
+				Product:  getProduct(itemInvoice, listsSiigo),
+				Quantity: itemInvoice.Quantity,
+				Price:    itemInvoice.Price,
+				Total:    itemInvoice.Total,
 			})
 		}
 		listBigQuery = append(listBigQuery, invoice)
@@ -71,33 +66,43 @@ func InsertInvoice(ctx context.Context, dataset bigquery.Dataset, listsSiigo sii
 	}
 }
 
+func getCustomerNameIdTypeName(item siigo_invoice.InvoiceSiigo, siigo siigo_model.SiigoData) string {
+	for _, typesSiigo := range siigo.Customers {
+		if typesSiigo.ID == item.Customer.ID {
+			return typesSiigo.IDType.Name
+		}
+	}
+	return ""
+}
+
+func getCustomerIdTypeCode(item siigo_invoice.InvoiceSiigo, siigo siigo_model.SiigoData) string {
+	for _, typesSiigo := range siigo.Customers {
+		if typesSiigo.ID == item.Customer.ID {
+			return typesSiigo.IDType.Code
+		}
+	}
+	return ""
+}
+
+func getCustomerName(item siigo_invoice.InvoiceSiigo, siigo siigo_model.SiigoData) string {
+	for _, typesSiigo := range siigo.Customers {
+		if typesSiigo.ID == item.Customer.ID {
+			return strings.Join(*typesSiigo.Name, " ")
+		}
+	}
+	return ""
+}
+
 func getProduct(invoice siigo_invoice.Item, listsSiigo siigo_model.SiigoData) bigquery_product.ProductSiigo {
 	var productBigQuery bigquery_product.ProductSiigo
 	for _, product := range listsSiigo.Products {
 		if invoice.Code == product.Code || invoice.ID == product.ID {
+			account := getAccountGroup(product.AccountGroup, listsSiigo.AccountGroup)
 			return bigquery_product.ProductSiigo{
-				ID:                  product.ID,
-				Code:                product.Code,
-				Name:                product.Name,
-				AccountGroup:        getAccountGroup(product.AccountGroup, listsSiigo.AccountGroup),
-				Type:                product.Type,
-				StockControl:        product.StockControl,
-				Active:              product.Active,
-				TaxClassification:   product.TaxClassification,
-				TaxIncluded:         product.TaxIncluded,
-				TaxConsumptionValue: util_bigquery.ConvertUint64PointerToInt64(product.TaxConsumptionValue),
-				Unit:                bigquery_product.Unit(product.Unit),
-				UnitLabel:           util_bigquery.SetStringToEmptyIfNil(product.UnitLabel),
-				Reference:           product.Reference,
-				Description:         product.Description,
-				AdditionalFields: bigquery_product.AdditionalFields{
-					Barcode: product.AdditionalFields.Barcode,
-				},
-				AvailableQuantity: product.AvailableQuantity,
-				Metadata: bigquery_product.Metadata{
-					Created:     product.Metadata.Created,
-					LastUpdated: util_bigquery.SetStringToEmptyIfNil(product.Metadata.LastUpdated),
-				},
+				Code:             product.Code,
+				Name:             product.Name,
+				AccountGroupID:   account.ID,
+				AccountGroupName: account.Name,
 			}
 		}
 	}
@@ -111,82 +116,4 @@ func getAccountGroup(group siigo_product.AccountGroup, accountGroups []siigo_cus
 		}
 	}
 	return bigquery_customer.AccountGroupSiigo{}
-}
-
-func getTaxes(taxes []siigo_invoice.Tax) []bigquery_invoice.Tax {
-	var taxesBigquery []bigquery_invoice.Tax
-	for _, tax := range taxes {
-		taxes = append(taxes, tax)
-	}
-	return taxesBigquery
-}
-
-func getCustomer(item siigo_invoice.InvoiceSiigo, siigo siigo_model.SiigoData) bigquery_customer.CustomerSiigo {
-	customer := bigquery_customer.CustomerSiigo{}
-	for _, typesSiigo := range siigo.Customers {
-		if typesSiigo.ID == item.Customer.ID {
-			customer = bigquery_customer.CustomerSiigo{
-				ID:             typesSiigo.ID,
-				Type:           typesSiigo.Type,
-				PersonType:     typesSiigo.PersonType,
-				IDType:         bigquery_customer.IDType(typesSiigo.IDType),
-				Identification: typesSiigo.Identification,
-				BranchOffice:   typesSiigo.BranchOffice,
-				CheckDigit:     util_bigquery.SetStringToEmptyIfNil(typesSiigo.CheckDigit),
-				Name:           strings.Join(*typesSiigo.Name, " "),
-				Active:         typesSiigo.Active,
-				VatResponsible: typesSiigo.VatResponsible,
-				Address:        bigquery_customer.Address(typesSiigo.Address),
-				CommercialName: util_bigquery.SetStringToEmptyIfNil(typesSiigo.CommercialName),
-				Metadata: bigquery_customer.Metadata{
-					Created:     typesSiigo.Metadata.Created,
-					LastUpdated: util_bigquery.SetStringToEmptyIfNil(typesSiigo.Metadata.LastUpdated),
-				},
-			}
-			for _, responsibility := range typesSiigo.FiscalResponsibilities {
-				customer.FiscalResponsibilities = append(customer.FiscalResponsibilities,
-					bigquery_customer.FiscalResponsibility(responsibility),
-				)
-			}
-			for _, phone := range typesSiigo.Phones {
-				customer.Phones = append(customer.Phones,
-					bigquery_customer.PhoneElement{
-						Indicative: util_bigquery.SetStringToEmptyIfNil(phone.Indicative),
-						Number:     phone.Number,
-						Extension:  util_bigquery.SetStringToEmptyIfNil(phone.Extension),
-					},
-				)
-			}
-			for _, contact := range typesSiigo.Contacts {
-				contactSiigo := bigquery_customer.Contact{
-					FirstName: contact.FirstName,
-					LastName:  contact.LastName,
-					Email:     contact.Email,
-				}
-				if contact.Phone != nil {
-					contactSiigo.Phone.Indicative = util_bigquery.SetStringToEmptyIfNil(contact.Phone.Indicative)
-					contactSiigo.Phone.Number = util_bigquery.SetStringToEmptyIfNil(contact.Phone.Number)
-				} else {
-					contactSiigo.Phone = bigquery_customer.ContactPhone{
-						Indicative: "",
-						Number:     "",
-					}
-				}
-				customer.Contacts = append(customer.Contacts)
-			}
-			return customer
-		}
-
-	}
-	return customer
-}
-
-func getDocument(item siigo_invoice.InvoiceSiigo, siigo siigo_model.SiigoData) bigquery_invoice.DocumentTypesSiigo {
-	document := bigquery_invoice.DocumentTypesSiigo{}
-	for _, typesSiigo := range siigo.DocumentType {
-		if typesSiigo.ID == item.Document.ID {
-			return bigquery_invoice.DocumentTypesSiigo(typesSiigo)
-		}
-	}
-	return document
 }
--- bigqueryPorvenir/repository/invoice/model/InvoiceSiigo.go
+++ bigqueryPorvenir/repository/invoice/model/InvoiceSiigo.go
@@ -1,66 +1,36 @@
 package bigquery_invoice
 
 import (
-	bigquery_customer "el_porvenir.com/cloudfunction/bigqueryPorvenir/repository/customer/model"
 	bigquery_product "el_porvenir.com/cloudfunction/bigqueryPorvenir/repository/product/model"
 )
 
 type InvoiceSiigo struct {
-	ID       string                          `json:"id"`
-	Document DocumentTypesSiigo              `json:"document"`
-	Prefix   string                          `json:"prefix"`
-	Number   int64                           `json:"number"`
-	Name     string                          `json:"name"`
-	Date     string                          `json:"date"`
-	Customer bigquery_customer.CustomerSiigo `json:"customer"`
-	Seller   int64                           `json:"seller"`
-	Total    float64                         `json:"total"`
-	Balance  int64                           `json:"balance"`
-	Items    []Item                          `json:"items"`
-	Payments []Payment                       `json:"payments"`
-	Stamp    Stamp                           `json:"stamp"`
-	Mail     Mail                            `json:"mail"`
-	Metadata Metadata                        `json:"metadata"`
-}
-
-type Document struct {
-	ID int64 `json:"id"`
+	ID                 string    `json:"id"`
+	Name               string    `json:"name"`
+	Date               string    `json:"date"`
+	CustomerName       string    `json:"customer_name"`
+	CustomerIdTypeCode string    `json:"customer_id_type_code"`
+	CustomerIdTypeName string    `json:"customer_id_type_name"`
+	Total              float64   `json:"total"`
+	Items              []Item    `json:"items"`
+	Payments           []Payment `json:"payments"`
+	StampStatus        string    `json:"stamp_status"`
+	StampCufe          string    `json:"stamp_cufe"`
+	MailStatus         string    `json:"mail_status"`
+	MailObservations   string    `json:"mail_observations"`
+	Created            string    `json:"created"`
+	LastUpdated        string    `json:"last_updated,omitempty"`
 }
 
 type Item struct {
-	Product     bigquery_product.ProductSiigo `json:"product"`
-	Quantity    float64                       `json:"quantity"`
-	Price       float64                       `json:"price"`
-	Description string                        `json:"description"`
-	Taxes       []Tax                         `json:"taxes,omitempty"`
-	Total       float64                       `json:"total"`
-}
-
-type Tax struct {
-	ID         int64   `json:"id"`
-	Name       string  `json:"name"`
-	Type       string  `json:"type"`
-	Percentage float64 `json:"percentage"`
-	Value      float64 `json:"value"`
-}
-
-type Mail struct {
-	Status       string `json:"status"`
-	Observations string `json:"observations"`
-}
-
-type Metadata struct {
-	Created     string `json:"created"`
-	LastUpdated string `json:"last_updated,omitempty"`
+	Product  bigquery_product.ProductSiigo `json:"product"`
+	Quantity float64                       `json:"quantity"`
+	Price    float64                       `json:"price"`
+	Total    float64                       `json:"total"`
 }
 
 type Payment struct {
 	ID    int64   `json:"id"`
 	Name  string  `json:"name"`
 	Value float64 `json:"value"`
-}
-
-type Stamp struct {
-	Status string `json:"status"`
-	Cufe   string `json:"cufe"`
 }
